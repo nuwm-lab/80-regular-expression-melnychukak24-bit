6d37a2e07e273494a50b47142c15abcd2feb8ad2
Сергій <melnychuk_ak24@nuwm.edu.ua>
2025-10-21 15:25:15 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
-﻿using System;
+using System;
+using System.Text.RegularExpressions;
 
-namespace LabWork
+namespace RegularExpressionExample
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
     class Program
     {
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Hello World!");
+            string text = " приклад: https://example.com/photo.jpg і ще https://site.com/image.gif.";
+
+            
+            string pattern = @"https?:\/\/\S+\.(jpg|png|gif)";
+
+            
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            
+            MatchCollection matches = regex.Matches(text);
+
+            if (matches.Count > 0)
+            {
+                Console.WriteLine(" Знайдено посилання на зображення:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Посилань на зображення не знайдено.");
+            }
         }
     }
 }
